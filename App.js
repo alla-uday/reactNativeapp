554639ca7ec759a6ac623dfefabde0bba5cc899e
Uday Alla <uday@fullstory.com>
2023-03-24 18:04:59 -0400

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FullStory from '@fullstory/react-native';
+
+FullStory.onReady().then(function(result) {
+  const replayStartUrl = result.replayStartUrl;
+  const replayNowUrl = result.replayNowUrl;
+  const sessionId = result.sessionId;
+  console.log('replayStartUrl', replayStartUrl);
+  console.log('replayNowUrl', replayNowUrl);
+  console.log('sessionId', sessionId);
+
+});	
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <><View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <View style={{ margin: 30, padding: 20, backgroundColor: '#c1f5cf' }} fsClass='fs-unmask'>
+        <Text style={{ fontSize: 16 }}>
+          Text has no global rules.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }}>
+          Views have no global rules.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }} fsClass='fs-mask'>
+          This text is locally masked.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }}>
+          The green view is locally unmasked.
+        </Text>
+        <View style={{
+          backgroundColor: 'pink',
+          padding: 10,
+          margin: 10,
+          borderWidth: 1,
+          borderColor: 'black'
+        }}>
+          <Text style={{ fontSize: 16 }} fsClass="fs-mask">
+            This text is locally masked.
+          </Text>
+          <Text style={{ fontSize: 16 }}>
+            The pink view has no local rules.
+          </Text>
+        </View>
+      </View>
+    </View><View style={{ margin: 30, padding: 20, backgroundColor: '#c1f5cf' }} fsClass='fs-unmask'>
+        <Text style={{ fontSize: 16 }}>
+          Text has no global rules.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }}>
+          BOOOOOOOM Views have no global rules.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }} fsClass='fs-mask'>
+          This text is locally masked.{'\n'}
+        </Text>
+        <Text style={{ fontSize: 16 }}>
+          The green view is locally unmasked.
+        </Text>
+        <View style={{
+          backgroundColor: 'pink',
+          padding: 10,
+          margin: 10,
+          borderWidth: 1,
+          borderColor: 'black'
+        }}>
+          <Text style={{ fontSize: 16 }} fsClass="fs-mask">
+            This text is locally masked.
+          </Text>
+          <Text style={{ fontSize: 16 }}>
+            The pink view has no local rules.
+          </Text>
+        </View>
+      </View></>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@fullstory/react-native', 
+      ['@fullstory/babel-plugin-annotate-react', { native: true }],
+    ]
   };
 };
